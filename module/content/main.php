b9b26f1777d7e8acfd75763ee01596de0d52405f
mkdgarp <natdanaichorchamp1@hotmail.com>
2020-12-26 22:00:58 +0700

    modify

--- module/content/main.php
+++ module/content/main.php
@@ -1,7 +1,24 @@
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            TEST push asdasdasd
+<div class="fixed-screen">
+    <div class="box-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                    <input class="form-control" type="text" placeholder="Test">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md">
+                    <button class="btn btn-primary btn-send">Button 1</button>
+                </div>
+            </div>
         </div>
     </div>
 </div>
+
+<script>
+$(document).ready(function() {
+    $(".btn-send").click(function() {
+        console.log("55");
+    });
+});
+</script>
